54c3a3597ce743b48fb00f248c785ff0ace55beb
long.nguyen25 <long.nguyen25@onemount.com>
2024-08-27 16:36:39 +0700

    update

---
+++ hello.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'nvlong',
+    'start_date': datetime(2024, 1, 25),
+    'catchup': False
+}
+
+dag = DAG(
+    'First DAG',
+    default_args = default_args,
+    schedule=timedelta(days=1)
+)
+
+t1 = BashOperator(
+    task_id = 'hello_world',
+    bash_command='echo "Hello World"',
+    dag = dag
+)
+
+t2 = BashOperator(
+    task_id = 'hello_dml',
+    bash_command='echo "Hello 1234566"',
+    dag = dag
+)
+
+t1 >> t2
